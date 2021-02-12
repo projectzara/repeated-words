55ab58c7e8dbfa24d9f313069cf370cc72dc150b
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-02-12 17:10:23 +0330

    minor update

--- engine.py
+++ engine.py
@@ -41,6 +41,6 @@ for word in final_list:
     print(f"Working on {word}")
     freq_dic[word] = final_list.count(word) 
 
-json_file = json.dump(freq_dic) 
+json_file = json.dumps(freq_dic) 
 with open("words.json", "w") as out_file:
     out_file.write(json)
