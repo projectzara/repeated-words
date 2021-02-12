d30767fc4c03a092a0e46a495aa1122675af7a5c
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-02-12 11:45:17 +0330

    Data preparation: the first step is done

--- engine.py
+++ engine.py
@@ -7,3 +7,13 @@ base_dir = "dataset"
 base_dir_listing = os.listdir(base_dir) 
 
 files_contents = []
+
+for directory in base_dir_listing:
+    if directory != ".DS_Store":
+        files = os.listdir(base_dir + "/" + directory)
+        for text_file in files:
+            if text_file != ".DS_Store":
+                f = open(base_dir + "/" + directory + "/" + text_file)
+                files_contents.append(f.readlines())
+
+print(len(files_contents))
